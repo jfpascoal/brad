0f3cafc16080460de186aa085003846d9fa1d007
JF Pascoal <jfpascoal@gmail.com>
2025-07-17 15:32:19 +0100

    add constant SECRETS_DIR with default location of secrets directory

--- src/bread/sql/__init__.py
+++ src/bread/sql/__init__.py
@@ -1,9 +1,10 @@
 import os
 
-DB_PATH =  os.path.abspath(os.path.join(
-    os.path.dirname(__file__), # bread/src/bread/sql
-    os.pardir, # bread/src/bread
-    os.pardir, # bread/src
-    os.pardir, # bread
-    os.path.join("data", "bread.db")
+SECRETS_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.pardir,
+    os.pardir,
+    os.pardir,
+    'docker',
+    'secrets'
 ))
