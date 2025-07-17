018d8500c069478801842ae17f2bd69b34b58382
JF Pascoal <jfpascoal@gmail.com>
2025-07-17 15:19:35 +0100

    update test tables to fix error

--- tests/sql/__init__.py
+++ tests/sql/__init__.py
@@ -1,20 +1,19 @@
-from decimal import Decimal
-from bread.sql.objects import Table, Column, PrimaryKey, Unique
+from bread.sql.objects import Table, Column, PrimaryKey, Unique, BigInt, Text, Integer, Numeric, \
+    GeneratedIdentityOptions
 
-# A standardized set of table definitions used across different test files.
-# This ensures consistency and avoids code duplication.
+
 TEST_TABLES = [
     Table('users').set_columns(
-        Column('id', int, row_id=True, autoincrement=True),
-        Column('name', str, not_null=True),
-        Column('age', int)  # Nullable column
+        Column('id', BigInt(), generated_identity=GeneratedIdentityOptions.ALWAYS),
+        Column('name', Text(), not_null=True),
+        Column('age', Integer())
     ).set_constraint(
         Unique(['name'])
     ),
     Table('products').set_columns(
-        Column('sku', str, not_null=True),
-        Column('name', str, not_null=True),
-        Column('price', Decimal, not_null=True)
+        Column('sku', Text(), not_null=True),
+        Column('name', Text(), not_null=True),
+        Column('price', Numeric(19, 2), not_null=True)
     ).set_constraint(
         PrimaryKey(['sku'])
     )
