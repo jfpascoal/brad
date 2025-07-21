9a33341b0580157710bca92d4f267e9e1153f1ff
JF Pascoal <jfpascoal@gmail.com>
2025-07-21 16:12:20 +0100

    add requirements file and automation scripts to set up the enviornment, and startup and shutdown the Postgres db

---
+++ scripts/setup_local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script sets up the local environment to run the Bread application, including:
+# 1) create virtual enviornment
+# 2) install dependencies
+# 3) build the application
+
+echo "Setting up the local environment..."
+
+# Create virtual environment
+echo "Creating virtual environment..."
+python3 -m venv development
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+# Build the application
+echo "Building the application..."
+pip install -e .
+
+echo "Setup complete."
