0ebd6355e04be0e6e78b7c4813dff69bb347d1a2
JF Pascoal <jfpascoal@gmail.com>
2025-08-13 23:31:19 +0100

    add data module

---
+++ python/src/brad/data/__init__.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+import yaml
+
+from brad import DATA_DIR, BACKUP_DIR
+
+
+HISTORY_FILE = os.path.join(DATA_DIR, "excel", "historical.ods")
+
+with open(os.path.join(DATA_DIR, "excel", "tabs.yaml"), 'rb') as f:
+    TABS = yaml.load(f.read(), Loader=yaml.Loader)
+
+with open(os.path.join(BACKUP_DIR, "reference.json"), 'rb') as f:
+    REFERENCE_DATA = json.load(f)
