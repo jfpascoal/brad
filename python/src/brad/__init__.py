528016b19115dc69b27deadcf21a5156592fb0cb
JF Pascoal <jfpascoal@gmail.com>
2025-08-13 23:26:56 +0100

    refactor: reorganize SECRETS_DIR import for consistency across modules

---
+++ python/src/brad/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+ROOT_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir,
+    os.path.pardir,
+    os.path.pardir
+))
+
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+BACKUP_DIR = os.path.join(DATA_DIR, 'backup')
+
+SECRETS_DIR = os.path.abspath(os.path.join(
+    ROOT_DIR,
+    'docker',
+    'secrets'
+))
