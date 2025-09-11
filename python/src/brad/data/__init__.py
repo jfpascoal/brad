ff092ac8dba35a80c822498bd73d8d2e985b4f6c
JF Pascoal <jfpascoal@gmail.com>
2025-09-11 22:29:07 +0100

    change YAML loader

--- python/src/brad/data/__init__.py
+++ python/src/brad/data/__init__.py
@@ -7,6 +7,6 @@ from brad import DATA_DIR, CONFIG_DIR
 HISTORY_FILE = os.path.join(DATA_DIR, "excel", "historical.ods")
 
 with open(os.path.join(CONFIG_DIR, "history.yaml"), 'rb') as f:
-    config = yaml.load(f.read(), Loader=yaml.Loader)
+    config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     TABS = config.get("tabs", {})
     FINANCIAL_PRODUCT_LABELS = config.get("financial_product_labels", {})
